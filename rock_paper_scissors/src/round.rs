7b06c659d3f85b4504783a7f8c3cb123b5f34167
Paul Stey <paulstey@gmail.com>
2022-12-05 22:13:06 -0500

    feat: solve prob2

--- rock_paper_scissors/src/round.rs
+++ rock_paper_scissors/src/round.rs
@@ -1,64 +1,69 @@
-
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct Round {
     elf: Shape,
     me: Shape,
     outcome: Outcome,
-    score: i32,
+    pub score: i32,
 }
 
 impl Round {
     pub fn new(round_string: &str) -> Round {
         let char_vec = round_string
             .chars()
-            .map(|c| c.to_string()) 
+            .map(|c| c.to_string())
             .collect::<Vec<String>>();
-       
+
         let elf_str = char_vec[0].clone();
-        let me_str = char_vec[2].clone();
+        let outcome_str = char_vec[2].clone();
 
         let elf = Shape::from(&elf_str);
-        let me = Shape::from(&me_str);
+        let outcome = match outcome_str.as_str() {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Tie,
+            "Z" => Outcome::Win,
+            _ => todo!(),
+        };
 
         let mut score = 0;
 
-        let outcome = match elf {
+        let me = match elf {
             Shape::Rock => {
-                if me == Shape::Paper {
+                if outcome == Outcome::Win {
                     score += 8;
-                    Outcome::Win
-                } else if me == Shape::Rock {
-                    score += 4; 
-                    Outcome::Tie
+                    Shape::Scissors
+                } else if outcome == Outcome::Tie {
+                    score += 4;
+                    Shape::Rock
                 } else {
                     score += 3;
-                    Outcome::Lose
+                    Shape::Paper
                 }
             }
-            
-            Shape::Paper=> {
-                if me == Shape::Scissors {
-                    score += 9; 
-                    Outcome::Win
-                } else if me == Shape::Paper {
-                    score += 5; 
-                    Outcome::Tie
+
+            Shape::Paper => {
+                if outcome == Outcome::Win {
+                    score += 9;
+                    Shape::Scissors
+                } else if outcome == Outcome::Tie {
+                    score += 5;
+                    Shape::Paper
                 } else {
                     score += 1;
-                    Outcome::Lose
+                    Shape::Rock
                 }
             }
 
             Shape::Scissors => {
-                if me == Shape::Rock{
-                    score += 7; 
-                    Outcome::Win
-                } else if me == Shape::Scissors {
-                    score += 6; 
-                    Outcome::Tie
+                if outcome == Outcome::Win {
+                    score += 7;
+                    Shape::Rock
+                } else if outcome == Outcome::Tie {
+                    score += 6;
+                    Shape::Scissors
                 } else {
                     score += 2;
-                    Outcome::Lose
+                    Shape::Paper
                 }
             }
         };
@@ -69,11 +74,10 @@ impl Round {
             outcome,
             score,
         }
-
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Outcome {
     Win,
     Lose,
@@ -86,7 +90,6 @@ pub enum Shape {
     Paper,
     Scissors,
 }
-
 
 impl Shape {
     pub fn from(letter: &str) -> Self {
